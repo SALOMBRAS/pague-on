27b1e2a8823d99f1ae76af54b53174f8926b9294
Pedro Salomão Camilo <pedrosalomao22099@gmail.com>
2026-08-24 17:08:52 -0300

    feat: POST /installments/:id/extra (parcela extra) (#8)
    * feat(installments): addExtraInstallment cria parcela extra e reconcilia venda/divida
    
    * feat(installments): rota POST /installments/:id/extra

--- src/controllers/installmentController.js
+++ src/controllers/installmentController.js
@@ -1,10 +1,11 @@
 const service = require('../services/installmentService');
-const { installmentPaySchema, idSchema } = require('../utils/validators');
+const { installmentPaySchema, addExtraSchema, idSchema } = require('../utils/validators');
 const { sendSuccess } = require('../utils/responseHelper');
 const { serialize } = require('../utils/serializers');
 const id = (req) => idSchema.parse(req.params).id;
 async function pay(req, res) { return sendSuccess(res, serialize(await service.payInstallment(req.user.id, id(req), installmentPaySchema.parse(req.body))), 'Pagamento da parcela registrado.'); }
 async function unpay(req, res) { return sendSuccess(res, serialize(await service.unpayInstallment(req.user.id, id(req))), 'Pagamento da parcela desfeito.'); }
 async function remind(req, res) { return sendSuccess(res, await service.remindInstallment(req.user.id, id(req)), 'Cobrança preparada.'); }
+async function extra(req, res) { const { amount, dueDate } = addExtraSchema.parse(req.body); return sendSuccess(res, serialize(await service.addExtraInstallment(req.user.id, id(req), amount, dueDate)), 'Parcela extra adicionada.'); }
 async function overdue(req, res) { return sendSuccess(res, serialize(await service.overdueInstallments(req.user.id))); }
-module.exports = { pay, unpay, remind, overdue };
+module.exports = { pay, unpay, remind, extra, overdue };

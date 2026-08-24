4fdf56c70ac66fd5b2f906418e7a579b1e1bbca9
Pedro Salomão Camilo <pedrosalomao22099@gmail.com>
2026-08-24 17:07:44 -0300

    feat(currency): agenda cron diario de atualizacao de cambio (#3)

--- src/services/cronScheduler.js
+++ src/services/cronScheduler.js
@@ -1,6 +1,7 @@
 const cron = require('node-cron');
 const notificationJobs = require('./notificationJobsService');
 const interestCalculator = require('./interestCalculator');
+const currencyService = require('./currencyService');
 
 function runSafely(name, job) {
   return async () => {
@@ -14,6 +15,7 @@ function startInternalCron() {
   cron.schedule('0 8 * * 1', runSafely('resumo semanal', () => notificationJobs.runDigest('WEEKLY_DIGEST')), { timezone: 'America/Sao_Paulo' });
   cron.schedule('0 9 1 * *', runSafely('resumo mensal', () => notificationJobs.runDigest('MONTHLY_DIGEST')), { timezone: 'America/Sao_Paulo' });
   cron.schedule('0 0 * * *', runSafely('recalcular juros', interestCalculator.recalculateAllInterest), { timezone: 'America/Sao_Paulo' });
+  cron.schedule('0 6 * * *', runSafely('atualizar câmbio', () => currencyService.updateExchangeRates()), { timezone: 'America/Sao_Paulo' });
   console.log('Cron interno ativado (America/Sao_Paulo).');
 }
 
